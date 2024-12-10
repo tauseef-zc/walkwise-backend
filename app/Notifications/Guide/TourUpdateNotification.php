b9626ac5a2f2a34c0fc085981ce2d2d8b98c1c2a
Tauseef Ahamed <tauseefahamed14@gmail.com>
2024-12-10 10:33:15 +0530

    feat - Tour Notification added

--- app/Notifications/Guide/TourUpdateNotification.php
+++ app/Notifications/Guide/TourUpdateNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TourUpdateNotification extends Notification
+class TourUpdateNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
